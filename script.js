2e34fe97a7b21ba71f49df45dd68bb6c1ee45ef3
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2020-09-16 20:41:29 +0200

    added button and inputfield

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
 function getWeather(name) {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q={city name}&appid={cadd248e697a9b875b9e032e97f46074}")
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q={name}&appid={cadd248e697a9b875b9e032e97f46074}")
         .then(response => response.json())
         .then(json => console.log(json))//actual JSON data as parameter //hier moet mijn functie komen die de data gaat tonen op het scherm
         .catch(err => console.error(err));
 }
 
-document.getElementById("btnJaune").addEventListener("click", function  () {
-    let name = document.getElementById("inputGroup-sizing-lg").value;
-    getPokemon(Id)
+document.getElementById("press").addEventListener("click", function  () {
+    let input = document.getElementById("cityinput").value();
+    getWeather(input)
 
 });
 
-f
+
 // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
 
 // fetch of pokemon fetch("https://pokeapi.co/api/v2/pokemon/"+id)
