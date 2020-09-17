b03ec33353048f5550b2cf89f8e80acb21ec5953
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2020-09-17 10:23:58 +0200

    added js script, fetchedand console logged the info for Antwerp

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
 function getWeather(name) {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q={name}&appid={cadd248e697a9b875b9e032e97f46074}")
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=antwerp&units=metric&appid=cadd248e697a9b875b9e032e97f46074")
         .then(response => response.json())
         .then(json => console.log(json))//actual JSON data as parameter //hier moet mijn functie komen die de data gaat tonen op het scherm
         .catch(err => console.error(err));
 }
 
 document.getElementById("press").addEventListener("click", function  () {
-    let input = document.getElementById("cityinput").value();
+    let input = document.getElementById("cityinput").value;
     getWeather(input)
 
 });
