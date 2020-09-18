3381829edf5451619eb74c710eeaede5320d171e
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2020-09-18 16:50:18 +0200

    last changes before weekend

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function getWeather(name) {
         .catch(err => console.error(err));
 }
 
-async function whitney(json) {
+async function whitney(json) { //temp info date is today temp1
 
     let i = 0;
     var d = new Date();
@@ -18,66 +18,77 @@ async function whitney(json) {
             aveTemp += json.list[i].main.temp;
         }
         aveTemp= aveTemp/8;
-        console.log(aveTemp);
-        // todo: weergave in html
+        //console.log(aveTemp);
+        document.getElementById("temp1").innerHTML = aveTemp.toFixed();
     } else if (h >= 3 && h < 6) {
         for (i = 0; i < 7; i++) {
             console.log(json.list[i].main.temp);
             aveTemp += json.list[i].main.temp;
         }
         aveTemp= aveTemp/7;
-        console.log(aveTemp);
-        // todo: weergave in html
+        //console.log(aveTemp);
+        document.getElementById("temp1").innerHTML = aveTemp.toFixed();
     } else if (h >= 6 && h < 9) {
         for (i = 0; i < 6; i++) {
             console.log(json.list[i].main.temp);
             aveTemp += json.list[i].main.temp;
         }
         aveTemp= aveTemp/6;
-        console.log(aveTemp);
-        // todo: weergave in html
+        //console.log(aveTemp);
+        document.getElementById("temp1").innerHTML = aveTemp.toFixed();
     } else if (h >= 9 && h < 12) {
         for (i = 0; i < 5; i++) {
             console.log(json.list[i].main.temp);
             aveTemp += json.list[i].main.temp;
         }
         aveTemp= aveTemp/5;
-        console.log(aveTemp);
-        // todo: weergave in html
+        //console.log(aveTemp);
+        document.getElementById("temp1").innerHTML = aveTemp.toFixed();
     } else if (h >= 12 && h < 15) {
         for (i = 0; i < 4; i++) {
             console.log(json.list[i].main.temp);
             aveTemp += json.list[i].main.temp;
         }
         aveTemp= aveTemp/4;
-        console.log(aveTemp);
-        // todo: weergave in html
+        document.getElementById("temp1").innerHTML = aveTemp.toFixed();
+        //console.log(aveTemp);
     } else if (h >= 15 && h < 18) {
         for (i = 0; i < 3; i++) {
             console.log(json.list[i].main.temp);
             aveTemp += json.list[i].main.temp;
         }
         aveTemp= aveTemp/3;
-        console.log(aveTemp);
-        // todo: weergave in html
+        //console.log(aveTemp);
+        document.getElementById("temp1").innerHTML = aveTemp.toFixed();
     } else if (h >= 18 && h < 21) {
         for (i = 0; i < 2; i++) {
             console.log(json.list[i].main.temp);
             aveTemp += json.list[i].main.temp;
         }
         aveTemp= aveTemp/2;
-        console.log(aveTemp);
-        // todo: weergave in html
+        //console.log(aveTemp);
+        document.getElementById("temp1").innerHTML = aveTemp.toFixed();
     } else {
         for (i = 0; i < 1; i++) {
             console.log(json.list[i].main.temp);
             aveTemp += json.list[i].main.temp;
         }
-        aveTemp= aveTemp/1;
-        console.log(aveTemp);
-        // todo: weergave in html
+        aveTemp;
+        //console.log(aveTemp);
+        document.getElementById("temp1").innerHTML = aveTemp.toFixed();
     }
+    console.log(i);//how much blocks of temp are visible
 }
+
+//temp day2 i= 4-12
+
+//temp day3 i= 13-21
+
+//temp day4 i= 22-30
+
+// temp day5 i= 31-39
+
+
 
 document.getElementById("press").addEventListener("click", function () {
     let input = document.getElementById("cityinput").value;
@@ -89,4 +100,10 @@ document.getElementById("press").addEventListener("click", function () {
 // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
 
 // for the temp response.data["list"][i]["main"]["temp"]
+
+// i= hoeveel blokken met temp er nog op de dag komen i=3 bijv dan nog 3 verschillende temp tussen 15.00 en 23.59
+
+//todo: hide API key, automatically change dates of the week, code to forcast days 2 untill 5 i
+//todo: see also notion comments
+
 
