0e910b37d57909912ca950db12823f4001f1254a
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2020-09-18 12:39:14 +0200

    caluculated average for one day

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
 function getWeather(name) {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q="+name+"&units=metric&appid=cadd248e697a9b875b9e032e97f46074")
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + name + "&units=metric&appid=cadd248e697a9b875b9e032e97f46074")
         .then(response => response.json())
         .then(json => whitney(json))//actual JSON data as parameter //hier moet mijn functie komen die de data gaat tonen op het scherm
         .catch(err => console.error(err));
@@ -9,67 +8,85 @@ function getWeather(name) {
 async function whitney(json) {
 
     let i = 0;
-    var t = new Date();
+    var d = new Date();
     var h = d.getHours();
-    if (h >=0 && h<3) {
-    for (i=0; i < 8; i++) {
-        console.log(json.list[i].main.temp); // ipv console log moet het in de browser zie hieronder
-        // document.getElementById("temp1")
-    }
+    var aveTemp = 0;
 
-    } else if (h>=3 && h<6) {
-        for (i=0; i < 7; i++) {
-            console.log(json.list[i].main.temp); // ipv console log moet het in de browser zie hieronder
-            // document.getElementById("temp1")
+    if (h >= 0 && h < 3) {
+        for (i = 0; i < 8; i++) {
+            console.log(json.list[i].main.temp);
+            aveTemp += json.list[i].main.temp;
         }
-    } else if (h>=6 && h<9) {
-        for (i = 0; i < 6; i++) ;
-        console.log(json.list[i].main.temp); // ipv console log moet het in de browser zie hieronder
-        // document.getElementById("temp1")
-    } else if (h>=9 && h<12) {
-        for (i = 0; i < 5; i++) ;
-        console.log(json.list[i].main.temp); // ipv console log moet het in de browser zie hieronder
-        // document.getElementById("temp1")
-    } else if (h>=12 && h<15) {
-        for (i = 0; i < 4; i++) ;
-        console.log(json.list[i].main.temp); // ipv console log moet het in de browser zie hieronder
-        // document.getElementById("temp1")
-    } else if (h>=15 && h<18) {
-        for (i = 0; i < 3; i++) ;
-        console.log(json.list[i].main.temp); // ipv console log moet het in de browser zie hieronder
-        // document.getElementById("temp1")
-    } else if (h>=18 && h<21) {
-        for (i = 0; i < 2; i++) ;
-        console.log(json.list[i].main.temp); // ipv console log moet het in de browser zie hieronder
-        // document.getElementById("temp1")
+        aveTemp= aveTemp/8;
+        console.log(aveTemp);
+        // todo: weergave in html
+    } else if (h >= 3 && h < 6) {
+        for (i = 0; i < 7; i++) {
+            console.log(json.list[i].main.temp);
+            aveTemp += json.list[i].main.temp;
+        }
+        aveTemp= aveTemp/7;
+        console.log(aveTemp);
+        // todo: weergave in html
+    } else if (h >= 6 && h < 9) {
+        for (i = 0; i < 6; i++) {
+            console.log(json.list[i].main.temp);
+            aveTemp += json.list[i].main.temp;
+        }
+        aveTemp= aveTemp/6;
+        console.log(aveTemp);
+        // todo: weergave in html
+    } else if (h >= 9 && h < 12) {
+        for (i = 0; i < 5; i++) {
+            console.log(json.list[i].main.temp);
+            aveTemp += json.list[i].main.temp;
+        }
+        aveTemp= aveTemp/5;
+        console.log(aveTemp);
+        // todo: weergave in html
+    } else if (h >= 12 && h < 15) {
+        for (i = 0; i < 4; i++) {
+            console.log(json.list[i].main.temp);
+            aveTemp += json.list[i].main.temp;
+        }
+        aveTemp= aveTemp/4;
+        console.log(aveTemp);
+        // todo: weergave in html
+    } else if (h >= 15 && h < 18) {
+        for (i = 0; i < 3; i++) {
+            console.log(json.list[i].main.temp);
+            aveTemp += json.list[i].main.temp;
+        }
+        aveTemp= aveTemp/3;
+        console.log(aveTemp);
+        // todo: weergave in html
+    } else if (h >= 18 && h < 21) {
+        for (i = 0; i < 2; i++) {
+            console.log(json.list[i].main.temp);
+            aveTemp += json.list[i].main.temp;
+        }
+        aveTemp= aveTemp/2;
+        console.log(aveTemp);
+        // todo: weergave in html
     } else {
-        for (i = 0; i < 1; i++) ;
-        console.log(json.list[i].main.temp); // ipv console log moet het in de browser zie hieronder
-        // document.getElementById("temp1")
+        for (i = 0; i < 1; i++) {
+            console.log(json.list[i].main.temp);
+            aveTemp += json.list[i].main.temp;
+        }
+        aveTemp= aveTemp/1;
+        console.log(aveTemp);
+        // todo: weergave in html
     }
-    //let temp = await whitney();
-   // let array = temp(json) aan Bert vragen of kan ik min & max temp gebruiken
-
-   // let displayHtml = document.getElementById("temp1").value;
 }
 
-document.getElementById("press").addEventListener("click", function  () {
+document.getElementById("press").addEventListener("click", function () {
     let input = document.getElementById("cityinput").value;
     getWeather(input);
 
 });
 
-// average temp=(temp_min + temp_max) / 2
 
 // api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
 
-// fetch of pokemon fetch("https://pokeapi.co/api/v2/pokemon/"+id)
-
 // for the temp response.data["list"][i]["main"]["temp"]
 
-
-
-
-
-
-// do I need to use submit (submit form) as an Event (listener
